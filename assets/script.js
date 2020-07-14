afd07aa5437b53c6ab05653b4152d0bbf55676b1
Jessica Feng <jessicafeng14@yahoo.com>
2020-07-13 21:24:33 -0700

    added functions to save user's input

--- assets/script.js
+++ assets/script.js
@@ -22,3 +22,62 @@ var interval = setInterval(function() {
                        .substring(0,3).toUpperCase());
   $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
 }, 100);
+
+function initPage() {
+
+    var init9 = JSON.parse(localStorage.getItem("09:00 am"));
+    nineAm.val(init9);
+  
+    var init10 = JSON.parse(localStorage.getItem("10:00 am"))
+    tenAm.val(init10);
+    
+    var init11 = JSON.parse(localStorage.getItem("11:00 am"))
+    elevenAm.val(init11);
+    
+    var init12 = JSON.parse(localStorage.getItem("12:00 pm"))
+    twelvePm.val(init12);
+    
+    var init1 = JSON.parse(localStorage.getItem("01:00 pm"))
+    onePm.val(init1);
+    
+    var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
+    twoPm.val(init2);
+    
+    var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
+    threePm.val(init3);
+   
+    var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
+    fourPm.val(init4);
+    
+    var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
+    fivePm.val(init5);
+  } 
+
+  function background () {
+      
+    $(".form-control").each(function () {
+        var timeTest = parseInt($(this).attr("id"));
+        hour = parseInt(hour);
+        if (hour > timeTest) {
+            $(this).addClass("past");
+        } else if (hour < timeTest) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present");
+        }
+    });
+  }
+
+  $(document).ready(function(){
+    initPage()
+    background()
+
+  $(".saveBtn").on("click", function(){
+    userInput = $(this).siblings(".form-control").val().trim();
+    console.log(userInput);
+    hourSpan = $(this).siblings(".input-group-prepend").text().trim();
+    console.log(hourSpan);
+    localStorage.setItem(hourSpan, JSON.stringify(userInput));
+  })
+
+});
